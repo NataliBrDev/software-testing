de840af5eba05209d86427d04a24306f1fe17347
NataliBrDev <natali.braznikova.dev@gmail.com>
2020-11-13 21:26:18 +0200

    Initial commit

---
+++ src/test/java/WebTEst.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class WebTEst {
+   private WebDriver driver;
+   private WebDriverWait wait;
+   @Before
+    public void start(){
+       driver = new ChromeDriver();
+       wait = new WebDriverWait(driver, 10);
+   }
+   @Test
+   public void firsttest(){
+      driver.get("http://localhost/litecart/admin/login.php");
+      driver.navigate().to("http://localhost/litecart/admin/login.php");
+      driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[1]/table/tbody/tr[1]/td[1]"));
+      WebElement element = driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[1]/table/tbody/tr[1]/td[2]/span/input"));
+      element.sendKeys("admin");
+      WebElement pass = driver.findElement(By.xpath("//*[@id='box-login'']/form/div[1]/table/tbody/tr[2]/td[2]/span/input"))
+       pass.sendKeys("admin");
+      WebElement button = driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[2]/button"));
+      button.click();
+
+
+   }
+
+}
