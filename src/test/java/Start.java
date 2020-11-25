507f612f00711e73d04bdd6ce16b41c96e2233e7
NataliBrDev <natali.braznikova.dev@gmail.com>
2020-11-25 17:19:53 +0200

    change do-while

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -8,16 +8,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
 
 public class Start {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         System.setProperty("webdriver.gecko.driver", "/home/natali/geckodriver/geckodriver");
         DesiredCapabilities dc = DesiredCapabilities.firefox();
         dc.setCapability("marionette", true);
-       WebDriver driver =  new FirefoxDriver(dc);
+        WebDriver driver = new FirefoxDriver(dc);
         //ChromeDriver driver =new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost/litecart/admin/login.php");
@@ -31,77 +35,14 @@ public class Start {
         WebElement button;
         button = driver.findElement(By.xpath("//div/form/div[2]/button"));
         button.click();
-        WebElement icon1 = driver.findElement(By.xpath("//div[3]/ul/li[1]/a/span[2]"));
-        icon1.click();
-        WebElement icon2 = driver.findElement(By.xpath("//div[3]/ul/li[2]/a/span[2]"));
-        icon2.click();
-        WebElement icon3 = driver.findElement(By.xpath("//div[3]/ul/li[3]/a/span[2]"));
-        icon3.click();
-        WebElement icon4 = driver.findElement(By.xpath("//div[3]/ul/li[4]/a/span[2]"));
-        icon4.click();
-        WebElement icon5 = driver.findElement(By.xpath("//div[3]/ul/li[5]/a/span[2]"));
-        icon5.click();
-        WebElement icon6 = driver.findElement(By.xpath("//div[3]/ul/li[6]/a/span[2]"));
-        icon6.click();
-        WebElement icon7 = driver.findElement(By.xpath("//div[3]/ul/li[7]/a/span[2]"));
-        icon7.click();
-        WebElement icon8 = driver.findElement(By.xpath("//div[3]/ul/li[8]/a/span[2]"));
-        icon8.click();
-        WebElement icon9 = driver.findElement(By.xpath("//div[3]/ul/li[9]/a/span[2]"));
-        icon9.click();
-        WebElement icon10 = driver.findElement(By.xpath("//div[3]/ul/li[10]/a/span[2]"));
-        icon10.click();
-        WebElement icon11 = driver.findElement(By.xpath("//div[3]/ul/li[11]/a/span[2]"));
-        icon11.click();
-        WebElement icon12 = driver.findElement(By.xpath("//div[3]/ul/li[12]/a/span[2]"));
-        icon12.click();
-        WebElement icon13 = driver.findElement(By.xpath("//div[3]/ul/li[13]/a/span[2]"));
-        icon13.click();
-        WebElement icon14 = driver.findElement(By.xpath("//div[3]/ul/li[14]/a/span[2]"));
-        icon14.click();
-        WebElement icon15 = driver.findElement(By.xpath("//div[3]/ul/li[15]/a/span[2]"));
-        icon15.click();
-        WebElement icon16 = driver.findElement(By.xpath("//div[3]/ul/li[16]/a/span[2]"));
-        icon16.click();
-        WebElement icon17 = driver.findElement(By.xpath("//div[3]/ul/li[17]/a/span[2]"));
-        icon17.click();
-        WebElement icon18 = driver.findElement(By.xpath("//div[3]/ul/li[18]/a/span[2]"));
-        icon18.click();
-        WebElement icon19 = driver.findElement(By.xpath("//div[3]/ul/li[19]/a/span[2]"));
-        icon19.click();
+        int i = 0;
+        do {
+            i++;
+            driver.findElement(By.xpath("//div[3]/ul/li[" + i + "]/a/span[2]")).click();
+            assertEquals(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true, true);
+        }
+        while (i >0 && i<=17 ) ;
 
-
-
-      icon1.click();
-        Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon2.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon3.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon4.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon5.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon6.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon7.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon8.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon9.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon10.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon11.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon12.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-     icon13.click();
-     Assert.assertTrue(String.valueOf(By.xpath("//table//tr/td[3]/h1")), true);
-
-     driver.quit();
-     driver.close();
-    }
-}
+    }}
 
 
--- src/test/java/TestGeozone.java
+++ src/test/java/TestGeozone.java
@@ -1,2 +1,62 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 public class TestGeozone {
+    public static void main(String[] args)  {
+
+        System.setProperty("webdriver.gecko.driver", "/home/natali/geckodriver/geckodriver");
+        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        dc.setCapability("marionette", true);
+        WebDriver driver =  new FirefoxDriver(dc);
+        //ChromeDriver driver =new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//*[@id=\"box-login\"]//tr[1]/td[1]"));
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"box-login\"]//span/input"));
+        element.sendKeys("admin");
+        WebElement pass = driver.findElement(By.xpath("//form//tr[2]/td[2]/span/input"));
+        pass.sendKeys("admin");
+        WebElement button;
+        button = driver.findElement(By.xpath("//div/form/div[2]/button"));
+        button.click();
+
+
+        WebElement icon3 = driver.findElement(By.xpath("//div[3]/ul/li[3]/a/span[2]"));
+        icon3.click();
+
+            String country1 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[2]/td[5]")).getAttribute("value");
+            String country2 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[3]/td[5]")).getAttribute("value");
+            String country3 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[4]/td[5]")).getAttribute("value");
+            String country4 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[5]/td[5]")).getAttribute("value");
+            String country5 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[6]/td[5]")).getAttribute("value");
+            String country6 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[7]/td[5]")).getAttribute("value");
+            String country7 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[8]/td[5]")).getAttribute("value");
+            String country8 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[9]/td[5]")).getAttribute("value");
+            String country9 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[10]/td[5]")).getAttribute("value");
+            String country10 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[11]/td[5]")).getAttribute("value");
+            String country11 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[12]/td[5]")).getAttribute("value");
+            String country12 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[13]/td[5]")).getAttribute("value");
+            String country13 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[14]/td[5]")).getAttribute("value");
+            String country14 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[15]/td[5]")).getAttribute("value");
+            String country15 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[16]/td[5]")).getAttribute("value");
+            String country16 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[17]/td[5]")).getAttribute("value");
+            String country17 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[18]/td[5]")).getAttribute("value");
+            String country18 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[19]/td[5]")).getAttribute("value");
+            String country19 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[20]/td[5]")).getAttribute("value");
+            String country20 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[21]/td[5]")).getAttribute("value");
+            String country21 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[22]/td[5]")).getAttribute("value");
+            String country22 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[23]/td[5]")).getAttribute("value");
+            String country23 = driver.findElement(By.xpath("//div/table//tr/td[3]//table//tr[24]/td[5]")).getAttribute("value");
+        }
+
+
 }
