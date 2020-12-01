810ff96239c33cd8127097cd65047bfe0cecd68b
NataliBrDev <natali.braznikova.dev@gmail.com>
2020-12-01 15:22:25 +0200

    firstcommit for backet

---
+++ src/test/java/AddPositionOnBox.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
+
+public class AddPositionOnBox {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.gecko.driver", "/home/natali/geckodriver/geckodriver");
+        DesiredCapabilities dc = DesiredCapabilities.firefox();
+        dc.setCapability("marionette", true);
+        WebDriver driver = new FirefoxDriver(dc);
+        //ChromeDriver driver =new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://litecart.stqa.ru/en/");
+        driver.manage().window().maximize();
+
+int i;
+for (i=1; i=3 ; i++) {
+    driver.findElement(By.xpath("//div[3]/div/ul/li["+i+"]/a[1]")).click();
+    driver.findElement(By.xpath("//div[5]//table//tr/td/button")).click();
+    driver.wait(10);
+    driver.navigate().refresh();
+   }
+driver.findElement(By.xpath("//div/header/div[3]/div/a[3]")).click();
+
+driver.findElement(By.xpath("//div/ul/li[1]//div/p[5]/button")).click();
+        driver.wait(10);
+        driver.navigate().refresh();
+        driver.findElement(By.xpath("//div/ul/li[1]//div/p[4]/button"));
+        driver.wait(10);
+        driver.navigate().refresh();
+        driver.findElement(By.xpath("//div/ul/li//div/p[4]/button"));
+        driver.wait(10);
+        driver.navigate().refresh();
+    }
+}
