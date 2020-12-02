fe652ca107622941f3c2289a156dc87deb85676e
NataliBrDev <natali.braznikova.dev@gmail.com>
2020-12-02 22:25:44 +0200

    change test

--- src/test/java/CheckNamePage.java
+++ src/test/java/CheckNamePage.java
@@ -17,34 +17,39 @@ public class CheckNamePage {
         //ChromeDriver driver =new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://litecart.stqa.ru/en/");
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
 
 //a
-        driver.findElement(By.xpath("/div[2]/div[2]/div[4]/h3"));
-        driver.findElement(By.xpath("//div[4]//ul/li/a[1]/div[2]"));  //name
+        var elementNameText = driver.findElement(By.xpath("//div[2]/div[4]//ul/li/a[1]/div[2]")).getText();  //name
         driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[4]/div/ul/li/a[1]")).click();
-        driver.findElement(By.xpath("//div[2]//div[1]/h1"));  //name1
-        Assert.assertEquals( driver.findElement(By.xpath("//div[4]//ul/li/a[1]/div[2]")).getText(), driver.findElement(By.xpath("//div[2]//div[1]/h1")).getText() );
+        var elementName1Text = driver.findElement(By.xpath("//div[2]/div[2]/div[1]/h1")).getText();  //name1
+        Assert.assertEquals( elementNameText, elementName1Text);
 
-        driver.findElement(By.xpath("//div[2]//div[2]/div[2]//nav/ul/li[1]/a")).click();
+        driver.findElement(By.xpath("//div//div[2]/div/div[1]/nav/ul/li[1]/a")).click();
         //b
-        driver.findElement(By.xpath("//div[2]/div[4]//ul/li/a[1]/div[4]/strong"));   //price
-        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[4]/div/ul/li/a[1]")).click();
-        driver.findElement(By.xpath("//div[2]//div[2]/strong"));     //price 1
-        Assert.assertEquals(  driver.findElement(By.xpath("//div[2]/div[4]//ul/li/a[1]/div[4]/strong")).getText(),driver.findElement(By.xpath("//div[2]//div[2]/strong")).getText() );
+        var elPrice= driver.findElement(By.xpath("//div[4]/div/ul/li/a[1]/div[4]/s")).getText();
+        System.out.println(elPrice);
+        var elPriceRed= driver.findElement(By.xpath("//div[4]/div/ul/li/a[1]/div[4]/strong")).getText();
+        driver.findElement(By.xpath("//div[2]/div[4]/div/ul/li/a[1]")).click();;
+        var elPrice1 =driver.findElement(By.xpath("//div[2]//div[2]/div[2]/s")).getText();
+        var elPriceRed1= driver.findElement(By.xpath("//div[2]/div[2]/strong")).getText();
+       Assert.assertEquals(elPrice, elPrice1);
+       Assert.assertEquals(elPriceRed,elPriceRed1);
+
+
 //v
-      Assert.assertTrue(driver.findElement(By.xpath("//div[2]/div[2]/div[2]/s")).getCssValue("strike"), true);
+      Assert.assertEquals(driver.findElement(By.xpath("//div[2]/div[2]/div[2]/s")).getCssValue("text-decoration-line"),"line-through");
+      Assert.assertEquals(driver.findElement(By.xpath("//div[2]/div[2]/div[2]/s")).getCssValue("color"),"rgb(102, 102, 102)");
 
 
 //g
-      Assert.assertTrue(driver.findElement(By.xpath("//div[2]/div[2]/strong")).getCssValue("color"),true);
+        Assert.assertEquals(driver.findElement(By.xpath("//div[2]/div[2]/strong")).getCssValue("color"),"rgb(204, 0, 0)");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[2]/div[2]/strong")).getCssValue("color"),"rgb(204, 0, 0)");
+
 
 //d
-        driver.findElement(By.xpath("//div[2]/div[2]/div[2]/s"));
-        driver.findElement(By.xpath("//div[2]/div[2]/div[2]/s"));
-        Assert.assertNotEquals(driver.findElement(By.xpath("//div[2]/div[2]/div[2]/s")).getSize(), driver.findElement(By.xpath("//div[2]/div[2]/div[2]/s")).getSize());
-
-
+       Assert.assertEquals( driver.findElement(By.xpath("//div[2]/div[2]/strong")).getCssValue("color"),"rgb(204, 0, 0)");
+       Assert.assertEquals(driver.findElement(By.xpath("//div[2]/div[2]/strong")).getCssValue("font-weight"),"700");
 
 
     }
